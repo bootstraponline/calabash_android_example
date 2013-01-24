9da32bfd585b9f7ba9320adf86413d065d871606
bootstraponline <code@bootstraponline.com>
2013-01-24 12:32:31 -0500

    Ruby 1.8

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -7,20 +7,20 @@ end
 
 Then /^I enter my "(.+)" username$/ do |username|
   wait_for_activity 'LoginActivity'
-  query "AutoCompleteTextView id:'et_login'", setText: username
+  query "AutoCompleteTextView id:'et_login'", :setText => username
 end
 
 Then /^I enter my "(.+)" password$/ do |password|
   wait_for_activity 'LoginActivity'
-  query "EditText id:'et_password'", setText: password
+  query "EditText id:'et_password'", :setText => password
 end
 
 When /^I touch (.+)$/ do |button|
-  m = { login: "ActionMenuItemView id:'m_login'",
-        username: "TextView id:'tv_org_name' index:0",
-        gists: "TextView id:'tv_org_name' text:'Gists'",
-        issues: "TextView id:'tv_org_name' text:'Issue Dashboard'",
-        bookmarks: "TextView id:'tv_org_name' text:'Bookmarks'"
+  m = { :login => "ActionMenuItemView id:'m_login'",
+        :username => "TextView id:'tv_org_name' index:0",
+        :gists => "TextView id:'tv_org_name' text:'Gists'",
+        :issues => "TextView id:'tv_org_name' text:'Issue Dashboard'",
+        :bookmarks => "TextView id:'tv_org_name' text:'Bookmarks'"
       }
 
   touch m[button.to_sym]
